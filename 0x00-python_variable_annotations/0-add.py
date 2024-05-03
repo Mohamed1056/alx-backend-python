a909fc4b3a87693f520cb5ef511ea1571e4f3043
Mohamed Gamal <mg8033667@gmail.com>
2024-05-03 09:22:26 -0700

    adding readme file and the first file.

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 '''function to add two numbers.'''
 
 
 def add(a: float, b: float) -> float:
+    '''functionthat returns two numbers.'''
     return a + b
